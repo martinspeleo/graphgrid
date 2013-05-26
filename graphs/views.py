69622f07e0626ab737051207391aaf36e22f2041
Martin Green <martin.speleo@gmail.com>
2013-05-26 17:16:01 +0100

    improved graphs

--- graphs/views.py
+++ graphs/views.py
@@ -3,6 +3,8 @@ from graphs.models import *
 from patients.models import *
 import datetime
 
+dateformat = "%Y-%m-%d-%H-%M"
+
 def home(request):
     graphgrids = GraphGrid.objects.all()
     return render(request, 'graphgrids.html', {'graphgrids': graphgrids})
@@ -10,17 +12,18 @@ def home(request):
 def graphgrid(request, graphgrid):
     graphgrid = get_object_or_404(GraphGrid, name = graphgrid)
     return render(request, 'graphgrid.html', {'graphgrid': graphgrid})
+
 def vitalsVis(request, mrn):
     patient = get_object_or_404(Patient, mrn = mrn)
-    datetimeonedaytemp = datetime.datetime.now() - datetime.timedelta(1)
-    datetimeoneday = datetime.datetime.strftime(datetimeonedaytemp, "%Y-%m-%d-%H-%M")
-    datetimefivedaystemp = datetime.datetime.now() - datetime.timedelta(5) 
-    datetimefivedays = datetime.datetime.strftime(datetimefivedaystemp, "%Y-%m-%d-%H-%M")
-    datenow = datetime.datetime.strftime( datetime.datetime.now(), "%d/%m/%Y")
-    timenow = datetime.datetime.strftime( datetime.datetime.now(), "%H:%M")
+    datetimenow = datetime.datetime(2010,11,01) 
+    datetimeoneday = datetime.datetime.strftime(datetimenow - datetime.timedelta(1), dateformat)
+    datetimefivedays = datetime.datetime.strftime(datetimenow - datetime.timedelta(5), dateformat)
+    datetimezerodays = datetime.datetime.strftime(datetimenow, dateformat)
+    datenow = datetime.datetime.strftime( datetimenow, "%d/%m/%Y")
+    timenow = datetime.datetime.strftime( datetimenow, "%H:%M")
     try: 
         recentRR = "%0.f" % NumericObservation.objects.filter( patient = patient, observation_type__name = "Respiratory Rate" ).order_by("-datetime")[0].value
     except:
         recentRR = "-"
 	
-    return render(request, 'vitalsvis.html', {'patient': patient, 'datenow': datenow, 'timenow': timenow, 'datetimeoneday': datetimeoneday, 'datetimefivedays': datetimefivedays, 'recentRR': recentRR})
+    return render(request, 'vitalsvis.html', {'patient': patient, 'datenow': datenow, 'timenow': timenow, 'datetimeoneday': datetimeoneday, 'datetimefivedays': datetimefivedays, 'datetimezerodays': datetimezerodays, 'recentRR': recentRR, 'width': 300})
